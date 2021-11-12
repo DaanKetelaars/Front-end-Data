62217585e2524c1bafc50e3a229752f5f9aab21e
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-12 11:41:21 +0100

    changes: data.js

--- components/data.js
+++ components/data.js
@@ -1,5 +1,6 @@
 // fetch LastFM api
 let listenersArr = [];
+let artistArr = [];
 
 const getData = () => fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=netherlands&api_key=7ebc4d450f175fa75d3260763df487fb&format=json')
     .then(response => response.json())
@@ -8,22 +9,36 @@ const getData = () => fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettop
         let showArtists = getData.map((data) => data);
         showArtists.map(artist => {
             const listeners = artist.listeners;
-            if (typeof listeners === 'string') {
+            const artistName = artist.name;
+            if (typeof listeners === 'string' || typeof artistName === 'string') {
                 listenersArr.push(listeners);
+                artistArr.push(artistName);
             }
         });
-        return cleanData(listenersArr)
+        const combinedArr = artistArr.map((artist, index) => {
+            return {
+                artist: artist,
+                listeners: listenersArr[index],
+            }
+        });
+        const newCombinedArr = combinedArr.map(item => {
+            const group = {};
+            item.listeners = +item.listeners;
+            group[item.artist] = item.listeners;
+            return group;
+        })
+        cleanData(newCombinedArr)
+        console.log(newCombinedArr)
     })
     .catch((error) => {
         console.log(error)
     });
 
 // sort all values
-function cleanData(listenersArr) {
-    let StrToNum = listenersArr.map((i) => Number(i));
-    StrToNum = StrToNum.sort((a, b) => b - a);
-    StrToNum = Object.values(StrToNum);
-    return StrToNum;
+function cleanData(newCombinedArr) {
+    let sortData = newCombinedArr;
+    sortData = sortData.sort((a, b) => b - a);
+    return sortData;
 }
 async function returnData() {
     let data = await getData()
