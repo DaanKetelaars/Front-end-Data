f357e66570de2b22f31fa64d0d452192c2980a1f
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-16 15:50:35 +0100

    changes: remove app.copy.js

--- app copy.js
+++
@@ -1,8 +0,0 @@
-// // import data
-import returnData from './components/data.js'
-import createGraph from './components/createGraph.js'
-
-returnData()
-    .then(data => {
-        createGraph(data)
-    });
