2cc00c0097811d0b2304dbacf436e0ea90caaf10
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-17 11:50:33 +0100

    changes: transition + interactivity

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ d3.json('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=neth
 });
 
 function update(topFiftyData) {
-    console.log(topFiftyData);
     // set the dimensions and margins of the graph
     const margin = {
             top: 100,
@@ -35,8 +34,8 @@ function update(topFiftyData) {
             bottom: 90,
             left: 100
         },
-        width = 1920 - margin.left - margin.right,
-        height = 1080 - margin.top - margin.bottom;
+        width = 1280 - margin.left - margin.right,
+        height = 720 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     const svg = d3.select("body")
@@ -72,9 +71,22 @@ function update(topFiftyData) {
         .attr("x", d => x(d.artist))
         .attr("width", x.bandwidth())
         .attr("fill", "#69b3a2")
-        // no bar at the beginning thus:
         .attr("height", d => height - y(0)) // always equal to 0
         .attr("y", d => y(0))
+        .attr("data-amount", d => (d.listeners))
+        .on("mouseover", function (d, i) {
+            tooltip.html(`Data: ${d.srcElement.dataset.amount}`).style("visibility", "visible")
+            console.log(d);
+            d3.select(this)
+                .attr("fill", ("#69b3a2", -15));
+        }).on("mousemove", function () {
+            tooltip
+                .style("top", (event.y - 10) + "px")
+                .style("left", (event.x + 10) + "px");
+        }).on("mouseout", function () {
+            tooltip.html(``).style("visibility", "hidden");
+            d3.select(this).attr("fill", "#69b3a2");
+        })
 
     // Animation
     svg.selectAll("rect")
@@ -84,8 +96,20 @@ function update(topFiftyData) {
         .attr("y", d => y(d.listeners))
         .attr("height", d => height - y(d.listeners))
         .delay((d, i) => {
-            console.log(i);
             return i * 100
         })
+
+    // create tooltip element  
+    const tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "d3-tooltip")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("padding", "15px")
+        .style("background", "rgba(0,0,0,0.6)")
+        .style("border-radius", "5px")
+        .style("color", "#fff")
+        .text("a simple tooltip");
 
 }
