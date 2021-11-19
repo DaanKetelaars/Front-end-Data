44caa6304a90e1320a7a65e01a33e9a695a43d0b
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-19 11:10:18 +0100

    changes: npm install changes

--- barChart.js
+++ barChart.js
@@ -3,15 +3,17 @@ let listenersArr = [];
 let artistArr = [];
 
 // fetch data with d3.json
-d3.json('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=netherlands&api_key=7ebc4d450f175fa75d3260763df487fb&format=json').then((data) => {
+d3.json(
+    "http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=netherlands&api_key=7ebc4d450f175fa75d3260763df487fb&format=json"
+).then((data) => {
     let getData = data.topartists.artist; // get the correct array
     console.log(getData);
     let showArtists = getData.map((data) => data); // map over this array and find the specific keys
-    showArtists.map(artist => {
+    showArtists.map((artist) => {
         const listeners = artist.listeners;
         const artistName = artist.name;
         // push keys in 2 separate array's
-        if (typeof listeners === 'string' || typeof artistName === 'string') {
+        if (typeof listeners === "string" || typeof artistName === "string") {
             listenersArr.push(listeners);
             artistArr.push(artistName);
         }
@@ -22,16 +24,16 @@ d3.json('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=neth
         return {
             artist: artist,
             listeners: listenersArr[index],
-        }
+        };
     });
     // numbers as string values to return as number values
     let selectNum = combinedArr.map((x) => {
-        return x.listeners = +x.listeners;
+        return (x.listeners = +x.listeners);
     });
     // sort the array based on number value - high to low
     combinedArr.sort((a, b) => b.listeners - a.listeners);
     let topFiftyData = combinedArr;
-    barChart(topFiftyData)
+    barChart(topFiftyData);
 });
 
 // create a function to get data from .then above
@@ -41,13 +43,14 @@ function barChart(topFiftyData) {
             top: 50,
             right: 100,
             bottom: 100,
-            left: 100
+            left: 100,
         },
         width = 1280 - margin.left - margin.right,
         height = 720 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    const svg = d3.select("body")
+    const svg = d3
+        .select("body")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -55,11 +58,13 @@ function barChart(topFiftyData) {
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
     // X axis
-    const x = d3.scaleBand()
+    const x = d3
+        .scaleBand()
         .range([0, width])
-        .domain(topFiftyData.map(d => d.artist))
+        .domain(topFiftyData.map((d) => d.artist))
         .padding(0.2);
-    svg.append("g")
+    svg
+        .append("g")
         .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(x))
         .selectAll("text")
@@ -67,50 +72,53 @@ function barChart(topFiftyData) {
         .style("text-anchor", "end");
 
     // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, 6500000])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    const y = d3.scaleLinear().domain([0, 6500000]).range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y));
 
     // Bars
-    svg.selectAll("bars")
+    svg
+        .selectAll("bars")
         .data(topFiftyData)
         .join("rect")
-        .attr("x", d => x(d.artist))
+        .attr("x", (d) => x(d.artist))
         .attr("width", x.bandwidth())
         .attr("fill", "#1ed760")
-        .attr("height", d => height - y(0)) // always equal to 0
-        .attr("y", d => y(0))
-        .attr("data-amount", d => (d.listeners))
+        .attr("height", (d) => height - y(0)) // always equal to 0
+        .attr("y", (d) => y(0))
+        .attr("data-amount", (d) => d.listeners)
         // on mouse effects to create interactivity
         .on("mouseover", function (d, i) {
-            tooltip.html(`${d.srcElement.__data__.artist}: ${d.srcElement.dataset.amount}`).style("visibility", "visible")
-            console.log(d);
-            d3.select(this)
-                .attr("fill", ("#1ed760", "#1db954"));
-        }).on("mousemove", function () {
             tooltip
-                .style("top", (event.y - 10) + "px")
-                .style("left", (event.x + 10) + "px");
-        }).on("mouseout", function () {
+                .html(`${d.srcElement.__data__.artist}: ${d.srcElement.dataset.amount}`)
+                .style("visibility", "visible");
+            console.log(d);
+            d3.select(this).attr("fill", ("#1ed760", "#1db954"));
+        })
+        .on("mousemove", function () {
+            tooltip
+                .style("top", event.y - 10 + "px")
+                .style("left", event.x + 10 + "px");
+        })
+        .on("mouseout", function () {
             tooltip.html(``).style("visibility", "hidden");
             d3.select(this).attr("fill", "#1ed760");
-        })
+        });
 
     // Animation
-    svg.selectAll("rect")
+    svg
+        .selectAll("rect")
         .transition()
         .ease(d3.easeQuadOut)
         .duration(800)
-        .attr("y", d => y(d.listeners))
-        .attr("height", d => height - y(d.listeners))
+        .attr("y", (d) => y(d.listeners))
+        .attr("height", (d) => height - y(d.listeners))
         .delay((d, i) => {
-            return i * 100
-        })
+            return i * 100;
+        });
 
-    // create tooltip element  
-    const tooltip = d3.select("body")
+    // create tooltip element
+    const tooltip = d3
+        .select("body")
         .append("div")
         .attr("class", "d3-tooltip")
         .style("position", "absolute")
@@ -120,6 +128,5 @@ function barChart(topFiftyData) {
         .style("background", "#3860be")
         .style("border-radius", "5px")
         .style("color", "#fff")
-        .style("font-family", "sans-serif")
-
+        .style("font-family", "sans-serif");
 }
